bd66aca1329da071daf0ed37cf95a7f45329d961
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-25 01:03:53 +0200

    Setup & Refractor from the old repo

---
+++ src/main/java/edy/springframework/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package edy.springframework.repositories;
+
+import edy.springframework.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeRepository extends CrudRepository<Recipe, String> {
+}
